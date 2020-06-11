f7e7b8b7e0b737250cf14daa8a436700f0f95feb
Ray Jay <rayjay@MacBook-Pro.local>
2020-06-11 09:44:59 -0400

    I Am Rich App

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ray Jay on 6/10/20.
+//  Copyright © 2020 Ray Jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
